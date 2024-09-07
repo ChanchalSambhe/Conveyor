4b8b8bfa0b1fe869927cf66efeed3fbf2b7f26a1
chanchal <141823691+ChanchalSambhe@users.noreply.github.com>
2024-09-07 19:16:05 +0530

    Update Conveyor.bat

--- Conveyor.bat
+++ Conveyor.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files\Google\Chrome\Application\chrome.exe"  --app="http://192.168.1.44/STFS.php" 
+"C:\Program Files\Google\Chrome\Application\chrome.exe"  --app="http://192.168.1.61/STFS.php" 
